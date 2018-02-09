39170646eb6c4424b4b24bc3fcc89dacb092b9ab
柏超曾 <565618435@qq.com>
2018-02-09 09:42:02 +0800

    pod

--- uploadDemo-master/uploadDemo/uploadDemo/ViewController.swift
+++ uploadDemo-master/uploadDemo/uploadDemo/ViewController.swift
@@ -257,7 +257,7 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate, UINaviga
             //获取选取后的图片
             var pickedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
             
-            pickedImage = resizeImage(originalImg: pickedImage)
+          
             
             
             //转成jpg格式图片
@@ -422,66 +422,7 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate, UINaviga
         }
     }
     
-    
-    
-    func resizeImage(originalImg:UIImage) -> UIImage{
-        
-        //prepare constants
-        let width = originalImg.size.width
-        let height = originalImg.size.height
-        let scale = width/height
-        
-        var sizeChange = CGSize()
-        
-        if width <= 1280 && height <= 1280{ //a，图片宽或者高均小于或等于1280时图片尺寸保持不变，不改变图片大小
-            return originalImg
-        }else if width > 1280 || height > 1280 {//b,宽或者高大于1280，但是图片宽度高度比小于或等于2，则将图片宽或者高取大的等比压缩至1280
-            
-            if scale <= 2 && scale >= 1 {
-                let changedWidth:CGFloat = 1280
-                let changedheight:CGFloat = changedWidth / scale
-                sizeChange = CGSize(width: changedWidth, height: changedheight)
-                
-            }else if scale >= 0.5 && scale <= 1 {
-                
-                let changedheight:CGFloat = 1280
-                let changedWidth:CGFloat = changedheight * scale
-                sizeChange = CGSize(width: changedWidth, height: changedheight)
-                
-            }else if width > 1280 && height > 1280 {//宽以及高均大于1280，但是图片宽高比大于2时，则宽或者高取小的等比压缩至1280
-                
-                if scale > 2 {//高的值比较小
-                    
-                    let changedheight:CGFloat = 1280
-                    let changedWidth:CGFloat = changedheight * scale
-                    sizeChange = CGSize(width: changedWidth, height: changedheight)
-                    
-                }else if scale < 0.5{//宽的值比较小
-                    
-                    let changedWidth:CGFloat = 1280
-                    let changedheight:CGFloat = changedWidth / scale
-                    sizeChange = CGSize(width: changedWidth, height: changedheight)
-                    
-                }
-            }else {//d, 宽或者高，只有一个大于1280，并且宽高比超过2，不改变图片大小
-                return originalImg
-            }
-        }
-        
-        UIGraphicsBeginImageContext(sizeChange)
-        
-        //draw resized image on Context
-        originalImg.draw(in: CGRect.init(x: 0, y: 0, width: sizeChange.width, height: sizeChange.height))
-        
-        //create UIImage
-        let resizedImg = UIGraphicsGetImageFromCurrentImageContext()
-        
-        UIGraphicsEndImageContext()
-        
-        return resizedImg!
-        
-    }
-    
+ 
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
